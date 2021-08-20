054ae873c4cdf798d7ebf8263a4e8d1bcfe134b8
Gregory Marklew <greg_marklew@hotmail..com>
2021-08-20 10:49:44 +0100

    solution

---
+++ js/Game.js
@@ -0,0 +1,10 @@
+class Game {
+    constructor() {
+      this.car = {};
+      this.obstacles = [];
+      this.score = 0;
+      this.obstaclesFrequency = 0;
+      this.gameOver = false;
+      this.animationId = null;
+    }
+  }
